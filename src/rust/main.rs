6c7d6d8eaf1be3c1ab6dcead2326f77b24d9b3dd
Very-Exquisite-Potato <veryexquisitepotato@gmail.com>
2025-04-04 18:51:30 -0600

    Update main.rs

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -1,1 +1,13 @@
+use std::io;
 
+fn main() {
+  println!("Please enter in a variable");
+
+  let mut skibidi: String = String::new();
+
+  std::io::stdin()
+    .read_line(&mut skibidi)
+    .expect("Failed to read line");
+
+  println!("The user has entered: {}!", skibidi);
+}
