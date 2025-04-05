7043c8d396f9fda41b093ac8baef35160254ec86
Very-Exquisite-Potato <veryexquisitepotato@gmail.com>
2025-04-04 18:53:28 -0600

    Update main.rs

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -3,11 +3,17 @@ use std::io;
 fn main() {
   println!("Please enter in a variable");
 
-  let mut skibidi: String = String::new();
+  let mut input: String = String::new();
 
   std::io::stdin()
-    .read_line(&mut skibidi)
+    .read_line(&mut input)
     .expect("Failed to read line");
 
-  println!("The user has entered: {}!", skibidi);
+  println!("The user has entered: {}!", input);
+
+  if (input == "input") {
+    println!("The user has entered in input");
+  } else {
+    println!("The user has not entered in input!");
+  }
 }
